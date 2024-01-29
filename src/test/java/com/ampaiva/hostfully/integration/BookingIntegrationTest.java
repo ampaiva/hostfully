6afcc6b7435a260e89b265bd9eb6b918f9e2f9c5
Alexandre Paiva <alexandre.paiva@loadsmart.com>
2024-01-29 09:47:19 -0300

    Booking bad request

--- src/test/java/com/ampaiva/hostfully/integration/BookingIntegrationTest.java
+++ src/test/java/com/ampaiva/hostfully/integration/BookingIntegrationTest.java
@@ -10,8 +10,10 @@ import static io.restassured.RestAssured.given;
 
 public class BookingIntegrationTest extends BaseIntegrationTest {
 
+    private static final String BOOKINGS = "bookings";
+
     BookingIntegrationTest() {
-        super(BookingDto.class, "bookings", "booking");
+        super(BookingDto.class, BOOKINGS, "booking");
     }
 
     @Override
@@ -41,7 +43,7 @@ public class BookingIntegrationTest extends BaseIntegrationTest {
                 .contentType(ContentType.JSON)
                 .body("{ \"start\": \"2024-01-11\", \"end\": \"2024-01-19\", \"guest\": { \"id\": " + guestId + " }, \"property\": { \"id\": " + propertyId + " } }")
                 .when()
-                .post(API + "bookings")
+                .post(API + BOOKINGS)
                 .then()
                 .statusCode(HttpStatus.CREATED.value());
 
@@ -50,8 +52,25 @@ public class BookingIntegrationTest extends BaseIntegrationTest {
                 .contentType(ContentType.JSON)
                 .body("{ \"start\": \"2024-01-12\", \"end\": \"2024-01-14\", \"guest\": { \"id\": " + guestId + " }, \"property\": { \"id\": " + propertyId + " } }")
                 .when()
-                .post(API + "bookings")
+                .post(API + BOOKINGS)
                 .then()
                 .statusCode(HttpStatus.CONFLICT.value());
+    }
+
+    @Test
+    public void testInvalidDatesBooking() {
+
+        int propertyId = getPropertyId();
+
+        int guestId = getGuestId();
+
+        // Create
+        givenCreate(HttpStatus.BAD_REQUEST.value())
+                .contentType(ContentType.JSON)
+                .body("{ \"start\": \"2024-01-19\", \"end\": \"2024-01-18\", \"guest\": { \"id\": " + guestId + " }, \"property\": { \"id\": " + propertyId + " } }")
+                .when()
+                .post(API + BOOKINGS)
+                .then()
+                .statusCode(HttpStatus.BAD_REQUEST.value());
     }
 }
